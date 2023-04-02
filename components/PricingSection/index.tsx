6a7743b5ac190cb9682550c049194182b01b0be4
HanSeongLee <jkhanseong@gmail.com>
2023-04-02 17:05:27 +0900

    ADD | PricingSection component

---
+++ components/PricingSection/index.tsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import styles from './style.module.scss';
+import ToggleButton from 'components/ToggleButton';
+import cn from 'classnames';
+import PlanCardContainer from 'containers/PlanCardContainer';
+
+const PricingSection: React.FC = () => {
+    const [monthly, setMonthly] = useState<boolean>(false);
+
+    const onMonthlyToggle = () => {
+        setMonthly(!monthly);
+    };
+
+    return (
+        <section className={cn(styles.pricingSection)}>
+            <h2 className={styles.title}>
+                Our Pricing
+            </h2>
+            <ToggleButton className={styles.monthlyToggleButton}
+                          on={monthly}
+                          onClick={onMonthlyToggle}
+                          labels={{
+                              off: 'Annually',
+                              on: 'Monthly',
+                          }}
+            />
+            <PlanCardContainer className={styles.planCardContainer}
+                               monthly={monthly}
+            />
+        </section>
+    );
+};
+
+export default PricingSection;
