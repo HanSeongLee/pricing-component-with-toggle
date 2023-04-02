f3b35ee4c19447d021bb4c18828c5143ce6f79c5
HanSeongLee <jkhanseong@gmail.com>
2023-04-02 17:37:07 +0900

    ADD | Main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,23 @@
-import type { NextPage } from 'next'
+import type { NextPage } from 'next';
+import styles from './style.module.scss';
+import PricingSection from 'components/PricingSection';
+import Container from 'components/Container';
 
 const Home: NextPage = () => {
-  return (
-      <>
-        <main>
-        </main>
-      </>
-  );
+    return (
+        <>
+            <header>
+                <h1 className={styles.hidden}>
+                    Pricing component with toggle
+                </h1>
+            </header>
+            <main className={styles.main}>
+                <Container>
+                    <PricingSection />
+                </Container>
+            </main>
+        </>
+    );
 }
 
 export default Home
